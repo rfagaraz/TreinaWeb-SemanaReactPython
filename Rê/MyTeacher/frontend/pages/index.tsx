3f1b00516bf4e04de2bdc176a87ac0f2821a0516
rfagaraz <47521434+rfagaraz@users.noreply.github.com>
2022-10-06 20:09:33 -0300

    Commit final Aula01

--- Rê/MyTeacher/frontend/pages/index.tsx
+++ Rê/MyTeacher/frontend/pages/index.tsx
@@ -1,14 +1,47 @@
+import { Box } from '@mui/material'
 import type { NextPage } from 'next'
+import { Professor } from '../src/@types/professor'
 import Cabecalho from '../src/components/Cabecalho/Cabecalho'
+import Lista from '../src/components/Lista/Lista'
 
 
 
 const Home: NextPage = () => {
-  return (
-    <>
+  const professores: Professor[] = [
+    {
+      id:1,
+      nome:"João",
+      foto:"https://github.com/rfagaraz.png",
+      descricao:"Descrição do Professor 1",
+      valor_hora: 100
+    },
+    {
+      id:2,
+      nome:"Professor 2",
+      foto:"https://github.com/rfagaraz.png",
+      descricao:"Descrição do Professor 2",
+      valor_hora: 200
+    },
+    {
+      id:3,
+      nome:"Professor 3",
+      foto:"https://github.com/rfagaraz.png",
+      descricao:"Descrição do Professor 3",
+      valor_hora: 300
+    },
+    {
+      id:4,
+      nome:"Professor 4",
+      foto:"https://github.com/rfagaraz.png",
+      descricao:"Descrição do Professor 4",
+      valor_hora: 400
+    }
+  ]
 
-      <div>Conteúdo</div>
-    </>
+  return (
+    <Box sx={{backgroundColor: 'secondary.main'}}>
+      <Lista professores={professores}></Lista>
+    </Box>
   )
 }
 
