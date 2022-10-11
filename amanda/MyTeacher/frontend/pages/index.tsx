cfc028e90b5f077fc5efa58c0b3ed3cdbb093202
Amanda Falcão <amandaf.dias96@gmail.com>
2022-10-11 16:57:45 -0300

    Backend criado e integrado com frontend

--- amanda/MyTeacher/frontend/pages/index.tsx
+++ amanda/MyTeacher/frontend/pages/index.tsx
@@ -1,46 +1,81 @@
-import { Box } from "@mui/material";
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  Grid,
+  Snackbar,
+  TextField,
+} from "@mui/material";
 import type { NextPage } from "next";
 import { Professor } from "../src/@types/professor";
 import Lista from "../src/components/Lista/Lista";
+import { useIndex } from "../src/hooks/pages/useIndex";
 
 const Home: NextPage = () => {
-  const professores: Professor[] = [
-    {
-      id: 1,
-      nome: "Professor 1",
-      foto: "http://github.com/amdfd.png",
-      descricao: "Descrição do profssor 1",
-      valor_hora: 100,
-    },
-    {
-      id: 1,
-      nome: "Professor 2",
-      foto: "http://github.com/amdfd.png",
-      descricao: "Descrição do profssor 2",
-      valor_hora: 200,
-    },
-    {
-      id: 1,
-      nome: "Professor 3",
-      foto: "http://github.com/amdfd.png",
-      descricao: "Descrição do profssor 3",
-      valor_hora: 300,
-    },
-    {
-      id: 1,
-      nome: "Professor 4",
-      foto: "http://github.com/amdfd.png",
-      descricao: "Descrição do profssor 4",
-      valor_hora: 400,
-    },
-  ];
+  const {
+    listaProfessores,
+    nome,
+    setNome,
+    email,
+    setEmail,
+    professorSelecionado,
+    setProfessorSelecionado,
+    marcarAula,
+    mensagem,
+    setMensagem,
+  } = useIndex();
 
   return (
-    <>
+    <div>
       <Box sx={{ backgroundColor: "secondary.main" }}>
-        <Lista professores={professores}></Lista>
+        <Lista
+          professores={listaProfessores}
+          onSelect={(professor) => setProfessorSelecionado(professor)}
+        ></Lista>
       </Box>
-    </>
+
+      <Dialog
+        onClose={() => setProfessorSelecionado(null)}
+        open={professorSelecionado !== null}
+        fullWidth
+        PaperProps={{ sx: { p: 5 } }}
+      >
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              label="Digite o nome"
+              type="text"
+              fullWidth
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Digite o email"
+              type="email"
+              fullWidth
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Grid>
+        </Grid>
+        <DialogActions sx={{ mt: 5 }}>
+          <Button onClick={() => setProfessorSelecionado(null)}>
+            Cancelar
+          </Button>
+          <Button onClick={() => marcarAula()}>Marcar</Button>
+        </DialogActions>
+      </Dialog>
+
+      <Snackbar
+        message={mensagem}
+        open={mensagem.length > 0}
+        autoHideDuration={2500}
+        onClose={() => setMensagem("")}
+      />
+    </div>
   );
 };
 
